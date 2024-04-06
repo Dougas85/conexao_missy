648c6a7304088abd764a68d1a4efe55d14a2da86
Dougas85 <149700231+Dougas85@users.noreply.github.com>
2024-04-05 21:07:46 -0300

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,82 @@
+# questions.py
+
+QUESTIONS = [
+    {
+        "id": 0,
+        "question": "Qual é a capital de Roraima?",
+        "options": ["Teresina", "Rio Branco", "Boa Vista", "Porto Velho"],
+        "answer": "Boa Vista"
+    },
+    {
+        "id": 1,
+        "question": "Quem pintou a Mona Lisa?",
+        "options": ["Vincent Van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Michelangelo"],
+        "answer": "Leonardo da Vinci"
+    },
+    {
+        "id": 2,
+        "question": "Com quantos Anos Moisés morreu?",
+        "options": ["100 anos", "125 anos", "120 anos", "130 anos"],
+        "answer": "120 anos"
+
+    },
+    {
+        "id": 3,
+        "question": "Quem foi o primeiro homem criado por Deus?",
+        "options": ["Moisés", "Abraão", "Adão", "Noé"],
+        "answer": "Adão"
+    },
+    {
+        "id": 4,
+        "question": "Quem construiu a arca que salvou sua família e vários animais de um grande dilúvio?",
+        "options": ["Jonas", "Salomão", "Noé", "Josué"],
+        "answer": "Noé"
+    },
+    {
+        "id": 5,
+        "question": "Quem foi engolido por um grande peixe, conforme a narrativa bíblica?",
+        "options": ["Jonas", "Pedro", "Paulo", "João"],
+        "answer": "Jonas"
+     },
+
+    {
+        "id": 6,
+        "question": "Qual é o maior mandamento segundo Jesus?",
+        "options": [
+            "Amar a Deus sobre todas as coisas",
+            "Não terás outros deuses além de mim",
+            "Amarás o teu próximo como a ti mesmo",
+            "Honrar pai e mãe"],
+        "answer": "Amar a Deus sobre todas as coisas" 
+    },
+
+    {
+        "id": 7,
+        "question": "Qual desses livros é um livro profético do Antigo Testamento?",
+        "options": ["Gênesis", "Êxodo", "Daniel", "Atos"],
+        "answer": "Daniel"
+    },
+
+    {
+        "id": 8,
+        "question": "Quem foi vendido por seus irmãos e acabou se tornando governador do Egito?",
+        "options": ["José", "Benjamim", "Ruben", "Judas"],
+        "answer": "José"
+    },
+
+    {
+        "id": 9,
+        "question": "Quem foi o primeiro dos juízes de Israel a liderar uma batalha contra os cananeus?",
+        "options": ["Gideão", "Otniel", "Débora", "Jefté"],
+        "answer": "Otniel"
+    },
+    
+    {
+        "id": 10,
+        "question": "Quem Escreveu o Livro de Hebreus no Novo Testamento?",
+        "options": ["Paulo", "Apolo", "Barnabé", "Autor desconhecido"],
+        "answer": "Autor desconhecido"
+    }
+]
+
+
